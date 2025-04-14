029962f2eb9489d92306b468f70d45616e1d0d4a
ajayshekhawat1803 <ajayshekhawat1803@gmail.com>
2025-04-14 22:16:14 +0530

    Added a auth middleware that will check user authentication for the protected routes

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+
+const authMiddleware = (req, res, next) => {
+    try {
+        const token = req.headers.authorization?.split(" ")[1];
+        if (!token) {
+            return res.status(401).json({ message: "Unauthorized", success: false, data: null });
+        }
+        jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+            if (err) {
+                return res.status(401).json({ message: "Session expired or Invalid token passed", success: false, data: null });
+            }
+            req.user = decoded;
+            next();
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Something went wrong", success: false, data: null });
+    }
+}
+
+export default authMiddleware;
