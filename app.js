2021039c3058e756742789a915ecbb331d6b6a3c
ajayshekhawat1803 <ajayshekhawat1803@gmail.com>
2025-05-03 17:38:20 +0530

    Updated database name

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ import authMiddleware from './middlewares/authMiddleware.js';
 import userRouter from './routers/userRouter.js';
 
 const port = process.env.PORT || 5000;
-const db = process.env.DB_URL || 'mongodb://localhost:27017/Real-time-Chat-App-default';
+const db = process.env.DB_URL || 'mongodb://localhost:27017/Cryspy-Talk';
 dotenv.config();
 
 // Database connection
