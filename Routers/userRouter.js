4fd9d686ad3e6cacc4521f99eb4327afc2c0a60c
ajayshekhawat1803 <ajayshekhawat1803@gmail.com>
2025-06-12 17:04:11 +0530

    Created a search user on basis of query component

--- Routers/userRouter.js
+++ Routers/userRouter.js
@@ -5,7 +5,7 @@ const userRouter = express.Router();
 
 userRouter.get('/get-logged-user', userController.getLoginUser)
 userRouter.get('/get-all-users', userController.getAllUsers)
-
+userRouter.get('/search', userController.userSearch)
 
 
 export default userRouter;
--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,3 +1,4 @@
+import friendshipModel from "../models/friendship.js";
 import userModel from "../models/user.js";
 import _ from "lodash";
 
@@ -16,6 +17,65 @@ class userCont {
         users = users.map((u) => _.pick(u, ["_id", "firstName", "lastName", "email", "profilePic"]));
         return res.status(200).json({ message: "Users fetched successfully", success: true, data: users });
     }
+
+    async userSearch(req, res) {
+        const query = req.query.q?.trim();
+        const userId = req.user.id;
+
+        try {
+            const regex = new RegExp(query || '', 'i');
+            let users = await userModel.find({
+                _id: { $ne: userId },
+                $or: [
+                    { firstName: { $regex: regex } },
+                    { lastName: { $regex: regex } },
+                    { username: { $regex: regex } },
+                    { email: { $regex: regex } }
+                ]
+            })
+                .select('_id firstName lastName username profilePic')
+                .limit(20)
+                .lean();
+
+            const friendships = await friendshipModel.find({
+                $or: [
+                    { senderId: userId },
+                    { receiverId: userId }
+                ]
+            }).lean();
+
+            const friendshipMap = {};
+            friendships.forEach(f => {
+                const otherId = f.senderId.toString() === userId ? f.receiverId.toString() : f.senderId.toString();
+                friendshipMap[otherId] = {
+                    status: f.status,
+                    isSender: f.senderId.toString() === userId
+                };
+            });
+
+            users = users.map(user => {
+                const f = friendshipMap[user._id.toString()];
+                const isFriend = f?.status === 'accepted';
+                const requestSent = f?.status === 'pending' && f.isSender;
+                return {
+                    ...user,
+                    isFriend,
+                    requestSent
+                };
+            });
+
+            return res.status(200).json({
+                message: "Users fetched successfully",
+                success: true,
+                data: users
+            });
+
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({ message: 'Server error' });
+        }
+    }
+
 }
 
 const userController = new userCont();
